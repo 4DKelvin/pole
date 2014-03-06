d83ca1348861d45efc253201dafe7ad073c7d3cb
maxzhang <zhangdaiping@gmail.com>
2014-03-06 18:29:09 +0800

    none

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: ['Gruntfile.js', 'bin/*', 'lib/**/*.js', 'test/**/*.js']
+        },
+
+        nodeunit: {
+            all: ['test/*.js']
+        },
+
+        watch: {
+            src: {
+                files: ['bin/*', 'lib/**/*.js'],
+                tasks: ['jshint']
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('test', ['jshint', 'nodeunit']);
+};
